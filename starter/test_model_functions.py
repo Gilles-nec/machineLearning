d8566a97c9cedbc792a0ccff7dd715982046469a
Gilles-nec <https://github.com/Gilles-nec>
2021-05-27 16:40:15 +0100

    heruko deploy

--- starter/test_model_functions.py
+++ starter/test_model_functions.py
@@ -1,5 +1,4 @@
-#Tests on 3 model functions
-
+# Tests on 3 model functions
 import sklearn
 import numpy
 import pandas as pd
@@ -7,36 +6,38 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from starter.ml.data import process_data
 from starter.ml.model import train_model
-from starter.ml.model import inference
 
-#load data
+# Load data
 data = pd.read_csv("data/clean_census.csv")
-
 train, test = train_test_split(data, test_size=0.20)
+
 
 def test_size_of_process_data():
     cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
     ]
-    assert len(process_data(train, categorical_features=cat_features, label="salary", training=True)) == 4
+    assert len(process_data(
+        train, categorical_features=cat_features,
+        label="salary", training=True)) == 4
+
 
 def test_type_of_train_model():
     cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
     ]
     X_train, y_train, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
@@ -46,16 +47,17 @@ def test_type_of_train_model():
     model = train_model(X_train, y_train)
     assert isinstance(model, sklearn.ensemble._forest.RandomForestClassifier)
 
+
 def test_type_of_predict():
     cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
     ]
     X_train, y_train, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
